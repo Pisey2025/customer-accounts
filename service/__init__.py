ad87227f7f817b56e87756b27ab3452fb11eb9c5
Pisey2025 <pokpisey2017@gmail.com>
2026-02-07 15:12:48 +0700

    Add Talisman security headers configuration

--- service/__init__.py
+++ service/__init__.py
@@ -1,1 +1,14 @@
-# service package
+from flask import Flask
+from flask_talisman import Talisman
+
+def create_app():
+    app = Flask(__name__)
+
+    Talisman(
+        app,
+        content_security_policy={
+            "default-src": "'self'"
+        }
+    )
+
+    return app
